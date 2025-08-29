b01f12f8218301bc1fe0df768944f48cc99d8143
Filipe Leal <filipelealtec@gmail.com>
2025-08-29 15:35:09 -0300

    C/C++ 2025

--- beecrowd/1021 - Notas e Moedas/teste.cpp
+++ beecrowd/1021 - Notas e Moedas/teste.cpp
@@ -23,10 +23,10 @@ int main(){
   [45]  = 545%100; 45
   */
 
-  //100  
+  //100
   billCem = value/100;
   value = fmod(value, 100); //modulo (%) para float
-  
+
   //50
   billCem = value/50;
   value = fmod(value, 50);
@@ -59,9 +59,9 @@ int main(){
 
 /*
 
-Leia um valor de ponto flutuante com duas casas decimais. Este valor representa um valor monetário. A seguir, 
-calcule o menor número de notas e moedas possíveis no qual o valor pode ser decomposto. 
-As notas consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e 0.01. 
+Leia um valor de ponto flutuante com duas casas decimais. Este valor representa um valor monetário. A seguir,
+calcule o menor número de notas e moedas possíveis no qual o valor pode ser decomposto.
+As notas consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e 0.01.
 A seguir mostre a relação de notas necessárias.
 
 Entrada
