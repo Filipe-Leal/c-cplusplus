3591da172bf4f18371b393e99d06e6b821ddcd53
Filipe Leal <filipelealtec@gmail.com>
2025-09-04 16:40:49 -0300

    C/C++ 2025

--- beecrowd/1036 - Fórmula de Bhaskara/1036 - Formula de Bhaskara.cpp
+++ beecrowd/1036 - Fórmula de Bhaskara/1036 - Formula de Bhaskara.cpp
@@ -1,36 +1,28 @@
 #include <iostream>
 #include <math.h>
-
-void bhaskara(float a_func, float b_func, float c_func){
-  float delta=0, xPositivo=0, xNegativo=0;
-
-  delta=pow(b_func, 2)-4*a_func*c_func;
-  printf("%s", delta);
-  /*if(delta>0 || delta==0){
-    xPositivo = (-b_func+sqrt(delta))/2*a_func;
-    xNegativo = (-b_func-sqrt(delta))/2*a_func;
-
-    std::cout << "R1 = " << xPositivo << std::endl;
-    std::cout << "R2 = " << xNegativo << std::endl;
-  } else {
-    std::cout << "Impossivel calcular";
-  }
-
-*/
-
-
-
-
-}
-
+#include <iomanip>
 
 int main(){
+
+  std::cout << std::setprecision(5) << std::fixed; //Definindo a precisão de numero decimais como 5 casas
+
   float a=0, b=0, c=0;
+  float delta=0, xPositivo=0.00, xNegativo=0;
 
   std::cin >> a >> b >> c;
 
-  bhaskara(a, b, c);
+  delta=(pow(b,2)-4*a*c);
 
+  xPositivo=(-b+(sqrt(delta)))/(2*a);
+  xNegativo=(-b-(sqrt(delta)))/(2*a);
+
+  if(a==0 || delta<0){
+    std::cout << "Impossivel calcular" << std::endl;
+  } else {
+
+    std::cout << "R1 = " << xPositivo << std::endl;
+    std::cout << "R2 = " << xNegativo << std::endl;
+  }
 
 
   return 0;
