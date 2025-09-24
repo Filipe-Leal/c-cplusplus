3b7afa012cd65000a77b1113ef572000a621a4a3
Filipe Leal <filipelealtec@gmail.com>
2025-09-24 07:25:23 -0300

    C/C++ 2025

--- beecrowd/1042 - Sort Simples/1042 - Sort Simples.cpp
+++ beecrowd/1042 - Sort Simples/1042 - Sort Simples.cpp
@@ -2,18 +2,31 @@
 
 #define N 3
 int main(){
-  int valores[N], ordenado[N] = {0, 0, 0};
+  int valores[N], original[N];
   int i, j, aux;
 
-  for(i=0;i<=(3-1);i++){
+  for(i=0;i<=(N-1);i++){
     std::cin >> valores[i];
+    original[i] = valores[i];
   }
-
-  //REVISAR ORDENAÇÃO
 
   for(i=0;i<=(N-1);i++){
-    std::cout << valores[i];
+    for(j=0;j<=(N-1);j++){
+      if(valores[i] < valores[j]){
+        aux = valores[i];
+        valores[i] = valores[j];
+        valores[j] = aux;
+      }
+    }
   }
 
+  for(i=0;i<=(N-1);i++){
+    std::cout << valores[i] << std::endl;
+  }
 
+  std::cout << std::endl;
+
+  for(i=0;i<=(N-1);i++){
+    std::cout << original[i] << std::endl;
+  }
 }
