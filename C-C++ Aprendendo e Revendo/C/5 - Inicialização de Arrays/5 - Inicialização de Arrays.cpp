05f3a8c88458fe5489f4909d0e753b0d49cdcf74
Filipe <filipelealtec@gmail.com>
2025-09-25 18:59:09 -0300

    C/C++ 2025

---
+++ C-C++ Aprendendo e Revendo/C/5 - Inicialização de Arrays/5 - Inicialização de Arrays.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  //Inicializando um vetor
+  float vetor[] = {1.5, 22.1, 4.56};
+
+  //Inicilizando uma matriz
+  int matriz1[2][3] = {1,2,3,4,5,6};
+
+  int matriz2[][3] ={{1,2,3},{4,5,6}};
+
+  system("pause");
+}
